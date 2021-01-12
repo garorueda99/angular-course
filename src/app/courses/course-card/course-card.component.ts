af90982fd99965cc2f18a14ecf52917eba91457b
Andres Rueda <andres.rueda@aheeva.com>
2021-01-12 14:46:12 -0500

    adding cardIndex

--- src/app/courses/course-card/course-card.component.ts
+++ src/app/courses/course-card/course-card.component.ts
@@ -1,64 +1,51 @@
 import {
-    AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
-    Attribute,
-    ChangeDetectionStrategy,
-    ChangeDetectorRef,
-    Component, DoCheck,
-    EventEmitter,
-    Input, OnChanges,
-    OnDestroy,
-    OnInit,
-    Output
-} from '@angular/core';
-import {Course} from '../../model/course';
-import {CoursesService} from '../courses.service';
-
-
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Attribute,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { Course } from "../../model/course";
+import { CoursesService } from "../courses.service";
 
 @Component({
-    selector: 'course-card',
-    templateUrl: './course-card.component.html',
-    styleUrls: ['./course-card.component.css']
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
-export class CourseCardComponent implements  OnInit {
+export class CourseCardComponent implements OnInit {
+  testAR = "Test Andres Rueda";
+  @Input()
+  course: Course;
 
-    @Input()
-    course: Course;
+  @Input()
+  cardIndex: number;
 
-    @Input()
-    cardIndex: number;
+  @Output("courseChanged")
+  courseEmitter = new EventEmitter<Course>();
 
-    @Output('courseChanged')
-    courseEmitter = new EventEmitter<Course>();
+  constructor(
+    private coursesService: CoursesService,
+    @Attribute("type") private type: string
+  ) {}
 
+  ngOnInit() {}
 
+  onTitleChanged(newTitle: string) {
+    this.course.description = newTitle;
+  }
 
-
-    constructor(private coursesService: CoursesService,
-                @Attribute('type') private type: string) {
-
-
-    }
-
-    ngOnInit() {
-
-
-    }
-
-
-
-    onTitleChanged(newTitle: string) {
-
-        this.course.description = newTitle;
-
-    }
-
-
-    onSaveClicked(description: string) {
-
-        this.courseEmitter.emit({...this.course, description});
-
-    }
-
-
+  onSaveClicked(description: string) {
+    this.courseEmitter.emit({ ...this.course, description });
+  }
 }
